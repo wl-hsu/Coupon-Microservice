dfb5fcf86f8dcb3774122c460fbab02203731e55
wl-hsu <wlhsu.github@gmail.com>
2023-03-08 14:12:32 -0500

    set up gateway and filter

---
+++ coupon-gateway/src/main/java/wlh/coupon/ZuulGatewayApplication.java
@@ -0,0 +1,18 @@
+package wlh.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Gateway application startup entry
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+}
