6df9ad024873a034e9cfdc110353b52738911be2
wl-hsu <wlhsu.github@gmail.com>
2023-03-08 23:19:49 -0500

    add gneneral exception handler class

---
+++ coupon-service/coupon-common/src/main/java/wlh/coupon/exception/CouponException.java
@@ -0,0 +1,11 @@
+package wlh.coupon.exception;
+
+/**
+ * Exception definitions for coupon microservice
+ */
+public class CouponException extends Exception {
+
+    public CouponException(String message) {
+        super(message);
+    }
+}
