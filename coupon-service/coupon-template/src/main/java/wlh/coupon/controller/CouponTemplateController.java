0e0539db653d80c69b2a4bb4c9ef0375974dcf3e
wl-hsu <wlhsu.github@gmail.com>
2023-03-10 17:23:07 -0500

    add and configure coupon template controller

---
+++ coupon-service/coupon-template/src/main/java/wlh/coupon/controller/CouponTemplateController.java
@@ -0,0 +1,82 @@
+package wlh.coupon.controller;
+
+import com.alibaba.fastjson.JSON;
+import wlh.coupon.entity.CouponTemplate;
+import wlh.coupon.exception.CouponException;
+import wlh.coupon.service.IBuildTemplateService;
+import wlh.coupon.service.ITemplateBaseService;
+import wlh.coupon.vo.CouponTemplateSDK;
+import wlh.coupon.vo.TemplateRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Coupon template related function controller
+ */
+@Slf4j
+@RestController
+public class CouponTemplateController {
+
+    /** Build Coupon Template Service */
+    private final IBuildTemplateService buildTemplateService;
+
+    /** Coupon Template Basic Service */
+    private final ITemplateBaseService templateBaseService;
+
+    @Autowired
+    public CouponTemplateController(IBuildTemplateService buildTemplateService,
+                                    ITemplateBaseService templateBaseService) {
+        this.buildTemplateService = buildTemplateService;
+        this.templateBaseService = templateBaseService;
+    }
+
+    /**
+     * Build a Coupon Template
+     * 127.0.0.1:7001/coupon-template/template/build
+     * 127.0.0.1:9000/coupon-microservice/coupon-template/template/build
+     * */
+    @PostMapping("/template/build")
+    public CouponTemplate buildTemplate(@RequestBody TemplateRequest request)
+            throws CouponException {
+        log.info("Build Template: {}", JSON.toJSONString(request));
+        return buildTemplateService.buildTemplate(request);
+    }
+
+    /**
+     * Construct Coupon Template Details
+     * 127.0.0.1:7001/coupon-template/template/info?id=1
+     * */
+    @GetMapping("/template/info")
+    public CouponTemplate buildTemplateInfo(@RequestParam("id") Integer id)
+            throws CouponException {
+        log.info("Build Template Info For: {}", id);
+        return templateBaseService.buildTemplateInfo(id);
+    }
+
+    /**
+     * Find all available coupon templates
+     * 127.0.0.1:7001/coupon-template/template/sdk/all
+     * */
+    @GetMapping("/template/sdk/all")
+    public List<CouponTemplateSDK> findAllUsableTemplate() {
+        log.info("Find All Usable Template.");
+        return templateBaseService.findAllUsableTemplate();
+    }
+
+    /**
+     * Get the mapping from template ids to CouponTemplateSDK
+     * 127.0.0.1:7001/coupon-template/template/sdk/infos
+     * */
+    @GetMapping("/template/sdk/infos")
+    public Map<Integer, CouponTemplateSDK> findIds2TemplateSDK(
+            @RequestParam("ids") Collection<Integer> ids
+    ) {
+        log.info("FindIds2TemplateSDK: {}", JSON.toJSONString(ids));
+        return templateBaseService.findIds2TemplateSDK(ids);
+    }
+}
