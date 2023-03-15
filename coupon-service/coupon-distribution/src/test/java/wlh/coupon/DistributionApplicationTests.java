a49668b2446664e0536502b08388d91790524104
wl-hsu <wlhsu.github@gmail.com>
2023-03-15 00:55:34 -0400

    add distribution sql file and unit test

---
+++ coupon-service/coupon-distribution/src/test/java/wlh/coupon/DistributionApplicationTests.java
@@ -0,0 +1,19 @@
+package wlh.coupon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Distribute microservice test case environment verification
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class DistributionApplicationTests {
+
+    @Test
+    public void contextLoad() {
+
+    }
+}
